63b470728ce69be7416c2c5c15c843238101dbb0
Davi Gledson <davigledson@outlook.com>
2024-01-10 11:23:55 -0300

    tetes de alteraçoes

--- anotaçoes.js
+++ anotaçoes.js
@@ -3,4 +3,6 @@
 // exemplo: git commit
 
 //git pull - sincronizar com o repositório remoto 
-//git log
+//git log  - log de todas as alterações (estão em ordem decrescente )
+
+//git mv - podemos mover/remomerar um arquivo
