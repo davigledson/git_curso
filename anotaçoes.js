9cc417de9b521962773b373f15b22e1ad2d9b731
Davi Gledson <davigledson@outlook.com>
2024-01-10 11:30:51 -0300

    nova alteracao

--- anotaçoes.js
+++ anotaçoes.js
@@ -5,4 +5,6 @@
 //git pull - sincronizar com o repositório remoto 
 //git log  - log de todas as alterações (estão em ordem decrescente )
 
-//git mv - podemos mover/remomerar um arquivo
+//git mv - podemos mover/remomenar um arquivo
+
+//git checkout <nome ou caminho do arquivo> - desfaz a alteraçao do arquivo
