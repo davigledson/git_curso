9cbabf73780694a6cdc8d2f7ff607e4965f06e26
Davi Gledson <davigledson@outlook.com>
2024-01-11 00:49:26 -0300

    anotacoes

--- anotaçoes.js
+++ anotaçoes.js
@@ -5,8 +5,19 @@
 //git pull - sincronizar com o repositório remoto 
 //git log  - log de todas as alterações (estão em ordem decrescente )
 
+//git rv - podemos temover um arquivo
+
+
 //git mv - podemos mover/remomenar um arquivo
 
 //git checkout <nome ou caminho do arquivo> - desfaz a alteraçao do arquivo (antes do commit)
 
 //git reset --hard <nome do branch>  (resetar tudo em um branch) - (desfaz todas as mudanças local)
+
+//branch - ramificação (é a forma do gir de separa as versões dos projetos)
+
+//git branch -lista os branch
+//git branch <cria um branch > -cria um branch
+//git branch -d  <nome do branch> - deletar o branch
+//git checkout  <nome do branch>  - mudar de branch
+//git checkout -b <nome do branch>  - mudar e criar um branch
