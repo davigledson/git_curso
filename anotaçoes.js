569d2956805ada51b51a7e38233b40b2289c2d20
Davi Gledson <davigledson@outlook.com>
2024-01-13 03:52:33 -0300

    melhorando

--- anotaçoes.js
+++ anotaçoes.js
@@ -80,3 +80,12 @@
 //git archive -podemos transformar o repo em um arquivo compactado (--format zip --output master_files.zip main)
 
 //exemplo git archive --format zip --output master_files.zip main
+
+//MELHORANDO OS COMMITS DO PROJETO
+//branchs com commits ruins
+//private branches
+//Onde criamos branches que não serão compartilhados no repositório, então podemos colocar qualquer commit;
+//Ao fim da solução do problema podemos fazer um rebase;
+ 
+//git rebase <atual> <funcionalidade> -i
+//Escolhemos os branches para excluir (squash) e renomear com (reword);
